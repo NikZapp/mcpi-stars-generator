6d7c0f6a36f1ee67f04096bf2c23ce237277949a
NikZapp <58770289+NikZapp@users.noreply.github.com>
2023-12-24 12:54:03 +0600

    Add script

---
+++ mcpi_stars.py
@@ -0,0 +1,40 @@
+from mcpi import minecraft
+from mcpi.vec3 import Vec3
+from random import randint
+
+mc = minecraft.Minecraft.create()
+
+def scatter(points, max_displacement, min_amount, max_amount):
+    new_points = []
+    for point in points:
+        for i in range(randint(min_amount, max_amount)):
+            m = max_displacement
+            displacement = Vec3(
+                randint(-m.x, m.x),
+                randint(-m.y, m.y),
+                randint(-m.z, m.z)
+            )
+            
+            new_points.append(point + displacement)
+    return new_points
+
+def place(points, block, id_):
+    for point in points:
+        if mc.getBlock(point) == 0:
+            mc.setBlock(point, block, id_)
+
+points = [Vec3(0, 60, 0)]
+
+points = scatter(points, Vec3(64, 10, 64), 6, 10)
+points = scatter(points, Vec3(20, 15, 20), 4, 10)
+points = scatter(points, Vec3(6, 6, 6), 3, 8)
+points = scatter(points, Vec3(3, 6, 3), 1, 2)
+
+place(points, 51, 1)
+
+points = scatter(points, Vec3(128, 10, 128), 6, 12)
+points = scatter(points, Vec3(20, 15, 20), 4, 10)
+points = scatter(points, Vec3(6, 6, 6), 3, 8)
+points = scatter(points, Vec3(3, 6, 3), 1, 2)
+
+place(points, 51, 1)
